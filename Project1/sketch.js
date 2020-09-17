33521729246095996d9106a29265d1cce76163b5
Sandra Johansson <60439594+sandrut8@users.noreply.github.com>
2020-09-16 22:32:58 -0400

    Update sketch.js

--- Project1/sketch.js
+++ Project1/sketch.js
@@ -3,7 +3,7 @@ var johnX, johnY;
 var johnSpeed =3;
 function preload() {
    john = loadImage("john.gif");
-    lucas =loadImage(lucas.gif);
+    lucas =loadImage("lucas.gif");
     
 }
 function setup() {
@@ -18,16 +18,16 @@ function draw(){
  background(220);
     
     if (keyIsDown(RIGHT_ARROW)){
-        jerryX += johnSpeed;
+        johnX += johnSpeed;
         }
     if (keyIsDown(LEFT_ARROW)){
-        jerryX -= johnSpeed;
+        johnX -= johnSpeed;
         }
     if (keyIsDown(UP_ARROW)){
-        jerryY -= johnSpeed;
+        johnY -= johnSpeed;
         }
     if (keyIsDown(DOWN_ARROW)){
-        jerryX += johnSpeed;
+        johnX += johnSpeed;
         }
     image(john, johnX, johnY);
 }
