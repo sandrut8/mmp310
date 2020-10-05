7855c09e3a1079e3cf6ed6d459e483a93590f8f2
Sandra Johansson <sandrutbell@gmail.com>
2020-10-05 19:00:48 -0400

    updated

--- Project1/sketch.js
+++ Project1/sketch.js
@@ -1,62 +1,70 @@
 var john, lucas;
 var johnX, johnY;
-var johnSpeed =3;
+var johnSpeed = 3;
 
 var roseImage, sunImage;
 
-var bgRoseX ={50, 100, 200, 350, 420, 506} ;
-var bgRoseY ={300, 250, 350, 250, 333, 356 };
-    
+var bgRoseX = [50, 100, 200, 350, 420, 506];
+var bgRoseY = [300, 250, 350, 250, 333, 356];
+
+
 function preload() {
-   john = loadImage("john.gif");
-    lucas =loadImage(lucas.gif);
-    roseImage =loadImage("rose.png");
-    sunImage =loadimage("sun.png")
+    john = loadImage("john.gif");
+    lucas = loadImage("lucas.gif");
+    roseImage = loadImage("rose.png");
+    sunImage = loadImage("sun.png")
 }
+
 function setup() {
-createCanvas(640, 480);
-    johnX =width/2;
-    johnY =height/2 + 100;
+    createCanvas(640, 480);
+    johnX = width / 2;
+    johnY = height / 2 + 100;
     imageMode(CENTER);
 
 }
-function draw(){
- background(220);
+
+function draw() {
+    background(100, 100, 255);
     //console.log(mouseX, mouseY);
-    
+
     //loop
-    
-    for (var cloudCounter = 0; cloudCounter < 5; cloudounter += 1) { 
-        image(sunImage, cloudCounter * 100,100)
-        }
-        for(let x = 0; x < width; x += 150) {
-            image(roseImage, x, 400);
-        
-            }
-    
-    for (let i = 0; < bgRoseX.length; i++){
-        image(roseImage, bgRoseX{i}, bgRoseY{i} );
+
+    for (var cloudCounter = 0; cloudCounter < 5; cloudCounter += 1) {
+        image(sunImage, cloudCounter * 100, 100)
+    }
+    for (let x = 0; x < width; x += 150) {
+        image(roseImage, x, 400);
+
+    }
+
+    for (let i = 0; i < bgRoseX.length; i++) {
+        image(roseImage, bgRoseX[i], bgRoseY[i]);
+    }
+
+    var johnIsWalking = false;
+
+    if (keyIsDown(RIGHT_ARROW)) {
+        johnX += johnSpeed;
+        johnIsWalking=true;
+    }
+    if (keyIsDown(LEFT_ARROW)) {
+        johnX -= johnSpeed;
+        johnIsWalking=true;
+    }
+    if (keyIsDown(UP_ARROW)) {
+        johnY -= johnSpeed;
+        johnIsWalking=true;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+        johnX += johnSpeed;
+        johnIsWalking=true;
     }
     
-    var johnISWalking =false;
-    
-    if (keyIsDown(RIGHT_ARROW)){
-        johnX += johnSpeed;
-        }
-    if (keyIsDown(LEFT_ARROW)){
-        johnX -= johnSpeed;
-        }
-    if (keyIsDown(UP_ARROW)){
-        johnY -= johnSpeed;
-        }
-    if (keyIsDown(DOWN_ARROW)){
-        johnX += johnSpeed;
-        }
-    image(john, johnX, johnY);
-}
 
-if (johnIsWalking){
-    image(johnWalk, johnX, johnY);
+
+    if (johnIsWalking) {
+        image(john, johnX, johnY);
     } else {
-        image(johnIdle, johnX, johnY);
+        image(lucas, johnX, johnY);
+    }
 }
