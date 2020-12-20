63ca4af4f1b4c38f4eff9f5a6cbfe76861d9bd91
Sandra Johansson <60439594+sandrut8@users.noreply.github.com>
2020-12-20 14:28:24 +0100

    Add files via upload

---
+++ project1-6/mapscene.js
@@ -0,0 +1,68 @@
+class MapScene {
+	constructor(){
+			this.background = [];
+			this.foreground = [];
+			this.roses =[];
+			this.suns = [];
+			this.portals = [];
+    }
+
+	}
+	
+
+	draw(){
+		background(220);
+		for (let i = 0; i < this.background.length; i++){
+			this.background[i].draw();
+		}
+
+		if (keyIsDown(RIGHT_ARROW)) {
+        player.x += player.speed;
+        player.isWalking = true;
+    }
+    if (keyIsDown(LEFT_ARROW)) {
+        player.x -= player.speed;
+        player.isWalking = true;
+    }
+    if (keyIsDown(UP_ARROW)) {
+        player.Y -= player.speed;
+        player.isWalking = true;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+        player.x += player.speed;
+        player.isWalking = true;
+    }
+
+    let enterPortal;
+    for (let i = 0; i < this.portals.length; i++) {
+    	this.portals[i].draw();
+
+    	if(this.portals[i].collide(player)){
+    		this.portals[i].drawText();
+
+    		if (keyIsDown(ENTER)) {
+    			enterPortal = this.portals[i].sceneToOpen;
+
+    		}
+    	}
+
+    	if (enterPortal){
+    		changeScene(enterPortal, currentScene);
+    	}
+
+    }
+
+    
+
+    		player.draw();
+
+    		for (let i = 0; i < this.roses.length; i++){
+    			this.clouds.(i).draw();
+    			this.clouds(i).update();
+    		}
+
+    		for (let i = 0; i < this.foreground.length; i++){
+    			this.foreground[i].draw();
+    		}
+	}
+}
---
+++ project1-6/sun.js
@@ -0,0 +1,14 @@
+class Sun extends GameObject {
+	update() {
+		
+		constructor(x, y) {
+			super(sunImage, x, y);
+		}
+	this.x += 2;
+	if (this.x > width +
+		this.width / 2){
+		this.x = -sthis.width / 2;
+		
+
+this.y +=random(-1, 1);
+}
